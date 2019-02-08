129a93f12bfd73747d5bb75cf1552e68d3f1f0f7
Lucas Santos <lucasmarodin@hotmail.com>
2019-02-07 21:48:01 -0500

    added flask app

---
+++ flask-app/app.py
@@ -0,0 +1,98 @@
+import os
+from flask import Flask, request, jsonify, render_template
+
+from keras.preprocessing import image
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+
+
+def prepare_image(img):
+    # Convert the image to a numpy array
+    img = image.img_to_array(img)
+    # Scale from 0 to 255
+    img /= 255
+    # Invert the pixels
+    img = 1 - img
+    # Flatten the image to an array of pixels
+    image_array = img.flatten().reshape(-1, 28 * 28)
+    # Return the processed feature array
+    return image_array
+
+
+@app.route('/', methods=['GET', 'POST'])
+
+def upload_file():
+    if request.method == 'POST':
+        print(request)
+
+        if request.files.get('file'):
+            # read the file
+            file = request.files['file']
+
+            # read the filename
+            filename = file.filename
+
+            # create a path to the uploads folder
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+            # Save the file to the uploads folder
+            file.save(filepath)
+
+            # Load the saved image using Keras and resize it to the
+            # mnist format of 28x28 pixels
+            image_size = (28, 28)
+            im = image.load_img(filepath, target_size=image_size,
+                                grayscale=True)
+
+            # Convert the 2D image to an array of pixel values
+            image_array = prepare_image(im)
+            print(image_array)
+
+            return "Data Pre-processing Complete!"
+    return '''
+<!DOCTYPE html>
+<html>
+<title>W3.CSS Template</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<style>
+body,h1 {font-family: "Raleway", sans-serif}
+body, html {height: 100%}
+.bgimg {
+  background-image: url('/static/images/genes.jpg');
+  min-height: 100%;
+  background-position: center;
+  background-size: cover;
+}
+</style>
+<body>
+
+<div class="bgimg w3-display-container w3-animate-opacity w3-text-white">
+  <div class="w3-display-topleft w3-padding-large w3-xlarge">
+    Logo
+  </div>
+  <div class="w3-display-middle">
+    <h1 class="w3-jumbo w3-animate-top">Choose X-Ray</h1>
+    <hr class="w3-border-grey" style="margin:auto;width:100%">
+    <p class="w3-large w3-center"></p>
+    <form method=post enctype=multipart/form-data>
+    <p><input type=file name=file>
+    <input type=submit value=Upload>
+    </form>
+
+  </div>
+  <div class="w3-display-bottomleft w3-padding-large">
+    Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a>
+  </div>
+</div>
+
+</body>
+</html>
+'''
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
